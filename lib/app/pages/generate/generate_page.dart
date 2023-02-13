5db54dc578dafa6f31b5a8a070bfaf4fe0ab9d20
Thanathorn M <thanathorn1073@gmail.com>
2023-02-13 12:13:15 +0700

    Squashed commit of the following:
    commit 22f88c2abfd8daada0e8cbaa16fcc4de2d52ebb3
    Author: Phattarapol L <63938605+PhattOZ@users.noreply.github.com>
    Date:   Thu Feb 9 16:03:35 2023 +0700
    
        Sign in page
    
    commit 33c06ca60af1e8e04a23e8ea54cc0f8c3bce61be
    Author: Phattarapol L <63938605+PhattOZ@users.noreply.github.com>
    Date:   Thu Feb 9 14:49:00 2023 +0700
    
        Preference survey (#3)
    
        * [contain error] preference survey on home
    
        * icon in pref 1
    
        * Update home.dart
    
    commit b6ca7d536581f322b42e445ad56d7e7e426f4662
    Author: Phattarapol L <63938605+PhattOZ@users.noreply.github.com>
    Date:   Tue Feb 7 13:33:52 2023 +0700
    
        [dependency] flutter_svg
    
    commit 1fdee60656a03a19b3dde44191b3f6898aa0fb77
    Author: Phattarapol L <63938605+PhattOZ@users.noreply.github.com>
    Date:   Sun Feb 5 16:18:52 2023 +0700
    
        Generate page
    
    commit 36f94fcf320578632ecfcfcb9374388fa64e1fd9
    Author: Phattarapol L <63938605+PhattOZ@users.noreply.github.com>
    Date:   Thu Feb 2 18:24:55 2023 +0700
    
        massive commit
    
        search, generate, layout for preference survey

---
+++ lib/app/pages/generate/generate_page.dart
@@ -0,0 +1,222 @@
+import 'package:filter_list/filter_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:trek_bkk_app/constants.dart';
+
+import '../../utils/limit_range_text_input_formatter.dart';
+
+List<String> tags = [
+  "home",
+  "alone",
+  "sad",
+  "veryverysad",
+  "okokokkkkkkkk",
+  "new line"
+];
+
+class GeneratePage extends StatefulWidget {
+  const GeneratePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<GeneratePage> createState() => _GeneratePageState();
+}
+
+class _GeneratePageState extends State<GeneratePage> {
+  int _maxDistSliderValue = 3;
+  List<String> selectedTagList = [];
+
+  late final TextEditingController _maxDistTextFieldController;
+
+  @override
+  void initState() {
+    super.initState();
+    _maxDistTextFieldController = TextEditingController();
+    _maxDistTextFieldController.text = "3";
+  }
+
+  @override
+  void dispose() {
+    _maxDistTextFieldController.dispose();
+    super.dispose();
+  }
+
+  void openFilterDialog() async {
+    await FilterListDialog.display<String>(
+      context,
+      useRootNavigator: false,
+      hideSelectedTextCount: true,
+      themeData: FilterListThemeData(context,
+          wrapSpacing: 8,
+          choiceChipTheme: const ChoiceChipThemeData(
+              selectedBackgroundColor: Color(lightColor),
+              selectedTextStyle: TextStyle(color: Colors.black)),
+          controlButtonBarTheme: ControlButtonBarThemeData(context,
+              buttonSpacing: 4,
+              controlButtonTheme: const ControlButtonThemeData(
+                  primaryButtonTextStyle: TextStyle(color: Colors.white),
+                  primaryButtonBackgroundColor: Color(primaryColor),
+                  backgroundColor: Color(lightColor),
+                  textStyle: TextStyle(color: Colors.black)))),
+      listData: tags,
+      selectedListData: selectedTagList,
+      choiceChipLabel: (tag) => tag,
+      validateSelectedItem: (list, val) => list!.contains(val),
+      onItemSearch: (tag, query) {
+        return tag.toLowerCase().contains(query.toLowerCase());
+      },
+      onApplyButtonClick: (list) {
+        setState(() {
+          selectedTagList = List.from(list!);
+        });
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> tags = selectedTagList
+        .map<Widget>((tag) => GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedTagList.remove(tag);
+                });
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: const Color(lightColor),
+                ),
+                child: Text(tag),
+              ),
+            ))
+        .toList();
+
+    tags.add(ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            shape: const StadiumBorder()),
+        onPressed: openFilterDialog,
+        child: const Text("+")));
+
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Container(
+        color: const Color(dividerColor),
+        padding: const EdgeInsets.all(24),
+        child: Row(children: [
+          const Text("some icon"),
+          const SizedBox(
+            width: 24,
+          ),
+          Flexible(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Your starting point",
+                      fillColor: Colors.white,
+                      filled: true),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Your destination",
+                      fillColor: Colors.white,
+                      filled: true),
+                )
+              ],
+            ),
+          )
+        ]),
+      ),
+      Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Maximum distance between two places"),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 96,
+                    child: TextFormField(
+                      textAlign: TextAlign.center,
+                      controller: _maxDistTextFieldController,
+                      decoration: InputDecoration(
+                          suffixText: "km",
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 8),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8))),
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                        LimitRangeTextInputFormatter(1, 10)
+                      ],
+                      onChanged: (value) {
+                        setState(() {
+                          _maxDistSliderValue = int.parse(value);
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: Slider(
+                      max: 10,
+                      divisions: 10,
+                      value: _maxDistSliderValue.toDouble(),
+                      label: _maxDistSliderValue.round().toString(),
+                      onChanged: ((value) {
+                        setState(() {
+                          _maxDistSliderValue = value.toInt();
+                          _maxDistTextFieldController.text =
+                              _maxDistSliderValue.toString();
+                        });
+                      }),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              const Text("Location types"),
+              const SizedBox(
+                height: 16,
+              ),
+              Wrap(
+                spacing: 8,
+                runSpacing: 8,
+                children: tags,
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              Center(
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 32, vertical: 16),
+                        shape: const StadiumBorder()),
+                    onPressed: () {
+                      print(_maxDistSliderValue);
+                      print(selectedTagList);
+                    },
+                    child: const Text("Generate")),
+              )
+            ],
+          )),
+    ]);
+  }
+}
